a901353440fed6c92918238295bb3375c5169faf
asebak <rhymeweaponlegend@gmail.com>
2024-02-20 06:25:17 -0500

    added integration to get times

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from pixoo import Pixoo
-import os
 import time
+
 import requests
+from bs4 import BeautifulSoup
+
+from pixoo import Pixoo
+
 timeout = 3600
+
 def retrieve_prayer_times():
-    return None
+    url = "https://us.mohid.co/sc/rockhill/greenvillemasjid/masjid/widget/api/index/?m=prayertimings"
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data)
+    prayer_times_table = soup.findAll('ul')[1]
+    new_data = parse_prayer_times_table(prayer_times_table)
+    return new_data
+
+def parse_prayer_times_table(elem):
+    result = {}
+    for sub in elem.find_all('li', recursive=False):
+        if sub.div is None:
+            continue
+        data = {}
+        prayer_name = str(sub.next_element.strip())
+        athan = sub.find("div", {"class": "prayer_azaan_div"})
+        if athan is not None:
+            data["athan"] = str(athan.text.strip())
+        iqama = sub.find("div", {"class": "prayer_iqama_div"})
+        if iqama is not None:
+            data["iqama"] = str(iqama.text.strip())
+        result[prayer_name] = data
+    return result
 
 def get_pixoo_devices():
     response = (requests.post(
@@ -30,8 +56,7 @@ def main():
     # Set up a connection and show the background
     pixoo = Pixoo(device['DevicePrivateIP'])
     pixoo.draw_image('background.png')
-    pixoo.get
-    settings = pixoo.get_settings()
+
     # pixoo.set_brightness(100) # Only used sometimes if the screen isn't bright enough
     pixoo.draw_text('-----', (20, 49), green)
     pixoo.draw_text('------', (20, 43), green)
